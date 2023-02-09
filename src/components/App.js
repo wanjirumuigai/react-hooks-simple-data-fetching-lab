be755fcc1c21d45965decb469dd46eefca4db81c
Pauline Muigai <wanjirumuigai@gmail.com>
2023-02-09 09:19:23 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogs, setDogs] = useState([]);
+  const [fetched, setFetched] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogs(data.message);
+
+        setFetched(true);
+      });
+  }, []);
+
+  if (!fetched) return <p>Loading ...</p>;
+
+  return <div>{<img src={dogs} key={dogs} alt="A Random Dog" />}</div>;
+}
+
+export default App;
